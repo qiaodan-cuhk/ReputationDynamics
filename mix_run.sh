20bedc1a1b9b3da0d97c58b6e8cce50edaf130cf
qiaodan-cuhk <danqiao@link.cuhk.edu.cn>
2022-11-30 20:38:13 +0800

    add mixed seeding+intrinsic reward program

---
+++ mix_run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+python Top_Down_mix.py --seed 293 --episode 100000 --norm 9 --alpha 0.6 --b 5 --k 1 &
+python Top_Down_mix.py --seed 1483 --episode 100000 --norm 9 --alpha 0.6 --b 5 --k 1 &
+python Top_Down_mix.py --seed 2345 --episode 100000 --norm 9 --alpha 0.6 --b 5 --k 1 &
+python Top_Down_mix.py --seed 4552 --episode 100000 --norm 9 --alpha 0.6 --b 5 --k 1 &
+python Top_Down_mix.py --seed 33 --episode 100000 --norm 9 --alpha 0.6 --b 5 --k 1 &
+python Top_Down_mix.py --seed 452 --episode 100000 --norm 9 --alpha 0.6 --b 5 --k 1 &
+python Top_Down_mix.py --seed 545354 --episode 100000 --norm 9 --alpha 0.6 --b 5 --k 1 &
+python Top_Down_mix.py --seed 723 --episode 100000 --norm 9 --alpha 0.6 --b 5 --k 1 &
+python Top_Down_mix.py --seed 29433 --episode 100000 --norm 9 --alpha 0.6 --b 5 --k 1 &
+python Top_Down_mix.py --seed 83 --episode 100000 --norm 9 --alpha 0.6 --b 5 --k 1 
